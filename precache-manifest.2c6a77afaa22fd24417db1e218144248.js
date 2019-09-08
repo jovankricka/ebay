dcfd82ebef2e7285b877f6b38f36df47f5aca58e
jovankricka <jovankricka@gmail.com>
2019-09-08 19:34:16 +0200

    Updates

---
+++ precache-manifest.2c6a77afaa22fd24417db1e218144248.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0c6a8157837f8ebaf5957011eafb9da",
+    "url": "/ebay/index.html"
+  },
+  {
+    "revision": "c61b6105b033197205a9",
+    "url": "/ebay/static/css/main.83cae7d3.chunk.css"
+  },
+  {
+    "revision": "d6d4c440ccdb55ca4d24",
+    "url": "/ebay/static/js/2.a2b77b74.chunk.js"
+  },
+  {
+    "revision": "c61b6105b033197205a9",
+    "url": "/ebay/static/js/main.e52c856e.chunk.js"
+  },
+  {
+    "revision": "bda437c4f1081a16d229",
+    "url": "/ebay/static/js/runtime~main.b02b8e3c.js"
+  },
+  {
+    "revision": "bc6729bcaa66d8a8c00d54fc18f15bac",
+    "url": "/ebay/static/media/logo.bc6729bc.png"
+  }
+]);
